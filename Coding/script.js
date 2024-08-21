345b327a7bc6dc6b19d9623f285d825af557e662
EdselAnjay <edjibranlesmana@gmail.com>
2024-08-21 15:06:27 +0800

    Add files via upload

---
+++ Coding/script.js
@@ -0,0 +1,17 @@
+const data1 = document.getElementById("data1");
+
+data1.addEventListener("click",function() {
+    alert("data diri Aydil telah diklik")
+})
+
+const data2 = document.getElementById("data2");
+
+data2.addEventListener("click",function() {
+    alert("data diri Nanda telah diklik")
+})
+
+const data3 = document.getElementById("data3");
+
+data3.addEventListener("click",function() {
+    alert("data diri Edsel telah diklik")
+})
